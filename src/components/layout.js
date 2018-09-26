d0a0e2c5e9a0f09fd794e4a5ce8b24090885be25
Brad Garropy <bradgarropy@gmail.com>
2018-09-26 14:18:48 -0500

    eslint formatting.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import {StaticQuery, graphql} from "gatsby"
 
-import Header from './header'
-import './layout.css'
+import Header from "./header"
+import "./layout.css"
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+const Layout = ({children}) => (
+    <StaticQuery
+        query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -17,35 +17,35 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
+        render={data => (
       <>
         <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
+            title={data.site.siteMetadata.title}
+            meta={[
+                {name: "description", content: "Sample"},
+                {name: "keywords", content: "sample, something"},
+            ]}
         >
-          <html lang="en" />
+            <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
+            style={{
+                margin: "0 auto",
+                maxWidth: 960,
+                padding: "0px 1.0875rem 1.45rem",
+                paddingTop: 0,
+            }}
         >
-          {children}
+            {children}
         </div>
       </>
-    )}
-  />
+        )}
+    />
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 }
 
 export default Layout
