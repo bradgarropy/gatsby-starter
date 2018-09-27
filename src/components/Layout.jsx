c682933c3c01590a2eb717109f3a4e2f77ae66c0
Brad Garropy <bradgarropy@gmail.com>
2018-09-27 11:52:48 -0500

    the great gatsby starter.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -25,20 +25,20 @@ const Layout = ({children}) => {
 
                 <title>gatsby starter</title>
 
-                <meta name="description" content="⚛️📄🚀 gatsby starter template"/>
+                <meta name="description" content="🥂 the great gatsby starter"/>
                 <meta name="keywords" content="gatsby, react, scss, eslint"/>
 
                 <meta name="twitter:card" content="summary_large_image"/>
                 <meta name="twitter:site" content="@bradgarropy"/>
                 <meta name="twitter:creator" content="@bradgarropy"/>
                 <meta name="twitter:title" content="gatsby starter"/>
-                <meta name="twitter:description" content="⚛️📄🚀 gatsby starter template"/>
+                <meta name="twitter:description" content="🥂 the great gatsby starter"/>
                 <meta name="twitter:image" content="https://res.cloudinary.com/bradgarropy/image/upload/q_auto,f_auto,ar_2:1,c_mpad,w_1800,b_white/gatsby-starter/gatsby-starter.png"/>
 
-                <meta property="og:url" content="https://great-gatsby-starter.netlify.com/"/>
+                <meta property="og:url" content="https://the-great-gatsby-starter.netlify.com/"/>
                 <meta property="og:type" content="website"/>
                 <meta property="og:title" content="gatsby starter"/>
-                <meta property="og:description" content="⚛️📄🚀 gatsby starter template"/>
+                <meta property="og:description" content="🥂 the great gatsby starter"/>
                 <meta property="og:image" content="https://res.cloudinary.com/bradgarropy/image/upload/q_auto,f_auto,ar_2:1,c_mpad,w_1800,b_white/gatsby-starter/gatsby-starter.png"/>
 
                 <link rel="icon" type="image/png" href={favicon}/>
