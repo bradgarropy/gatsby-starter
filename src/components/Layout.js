d97b5e5a14ada3b9f2d1f9adc6251d21a415911e
Brad Garropy <bradgarropy@gmail.com>
2018-12-13 20:19:29 -0600

    linter configs. update packages. added static directory.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,67 +7,70 @@ import Header from "./Header"
 import Footer from "./Footer"
 
 // favicon
-import favicon from "../images/gatsby-icon.png"
+import favicon from "../../static/icons/gatsby-icon.png"
 
 // styles
 import "../scss/Layout.scss"
 
-
 const Layout = ({children}) => {
-
     return (
-
         <React.Fragment>
-
             <Helmet>
-
                 <html lang="en"/>
 
                 <title>gatsby starter</title>
 
-                <meta name="description" content="🥂 the great gatsby starter"/>
+                <meta
+                    name="description"
+                    content="🥂 the great gatsby starter"
+                />
                 <meta name="keywords" content="gatsby, react, scss, eslint"/>
 
                 <meta name="twitter:card" content="summary_large_image"/>
                 <meta name="twitter:site" content="@bradgarropy"/>
                 <meta name="twitter:creator" content="@bradgarropy"/>
                 <meta name="twitter:title" content="gatsby starter"/>
-                <meta name="twitter:description" content="🥂 the great gatsby starter"/>
-                <meta name="twitter:image" content="https://res.cloudinary.com/bradgarropy/image/upload/q_auto,f_auto,ar_2:1,c_mpad,w_1800,b_white/gatsby-starter/gatsby-starter.png"/>
+                <meta
+                    name="twitter:description"
+                    content="🥂 the great gatsby starter"
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://res.cloudinary.com/bradgarropy/image/upload/q_auto,f_auto,ar_2:1,c_mpad,w_1800,b_white/gatsby-starter/gatsby-starter.png"
+                />
 
-                <meta property="og:url" content="https://the-great-gatsby-starter.netlify.com/"/>
+                <meta
+                    property="og:url"
+                    content="https://the-great-gatsby-starter.netlify.com/"
+                />
                 <meta property="og:type" content="website"/>
                 <meta property="og:title" content="gatsby starter"/>
-                <meta property="og:description" content="🥂 the great gatsby starter"/>
-                <meta property="og:image" content="https://res.cloudinary.com/bradgarropy/image/upload/q_auto,f_auto,ar_2:1,c_mpad,w_1800,b_white/gatsby-starter/gatsby-starter.png"/>
+                <meta
+                    property="og:description"
+                    content="🥂 the great gatsby starter"
+                />
+                <meta
+                    property="og:image"
+                    content="https://res.cloudinary.com/bradgarropy/image/upload/q_auto,f_auto,ar_2:1,c_mpad,w_1800,b_white/gatsby-starter/gatsby-starter.png"
+                />
 
                 <link rel="icon" type="image/png" href={favicon}/>
-
             </Helmet>
 
             <div className="layout">
-
                 <Header/>
 
-                <div className="content">
-                    {children}
-                </div>
+                <div className="content">{children}</div>
 
                 <Footer/>
-
             </div>
-
         </React.Fragment>
-
     )
-
 }
-
 
 Layout.propTypes = {
     children: PropTypes.node,
 }
-
 
 // export
 export default Layout
