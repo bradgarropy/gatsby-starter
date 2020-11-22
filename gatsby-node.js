d66197fd2a29b4ae1abf12801f7e82076171cae2
Brad Garropy <bradgarropy@gmail.com>
2020-11-21 22:43:42 -0600

    log out env.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,7 @@
 const path = require("path")
 
+console.log(process.env.TEST_SECRET)
+
 const onCreateWebpackConfig = ({stage, actions}) => {
     if (stage.startsWith("develop")) {
         actions.setWebpackConfig({
