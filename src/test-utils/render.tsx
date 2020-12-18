f4013af2fef72fba3e3bb5abfe39ab4f5c309d0f
Brad Garropy <bradgarropy@gmail.com>
2020-12-18 13:28:53 -0600

    fixing types.

--- src/test-utils/render.tsx
+++ src/test-utils/render.tsx
@@ -1,7 +1,8 @@
 import theme from "styles/theme"
 import PropTypes from "prop-types"
-import {render} from "@testing-library/react"
+import {ReactElement} from "react"
 import {ThemeProvider} from "styled-components"
+import {render, RenderOptions, RenderResult} from "@testing-library/react"
 
 const Providers = ({children}) => {
     return <ThemeProvider theme={theme}>{children}</ThemeProvider>
@@ -11,8 +12,10 @@ Providers.propTypes = {
     children: PropTypes.node,
 }
 
-const customRender = (ui, options) =>
-    render(ui, {wrapper: Providers, ...options})
+const customRender = (
+    ui: ReactElement,
+    options?: RenderOptions,
+): RenderResult => render(ui, {wrapper: Providers, ...options})
 
 export * from "@testing-library/react"
 export {customRender as render}
--- src/test-utils/setup.ts
+++ src/test-utils/setup.ts
@@ -1,33 +1,22 @@
+import "core-js/stable"
 import "jest-styled-components"
 import "@testing-library/jest-dom"
+import "regenerator-runtime/runtime"
 
-jest.mock("gatsby", async () => {
-    const React = await require("react")
+jest.mock("gatsby", () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const React = require("react")
     const gatsby = jest.requireActual("gatsby")
 
     return {
         ...gatsby,
         graphql: jest.fn(),
-        Link: jest
-            .fn()
-            .mockImplementation(
-                ({
-                    activeClassName,
-                    activeStyle,
-                    getProps,
-                    innerRef,
-                    partiallyActive,
-                    ref,
-                    replace,
-                    to,
-                    ...rest
-                }) =>
-                    React.createElement("a", {
-                        ...rest,
-                        href: to,
-                        gatsby: "true",
-                    }),
-            ),
+        Link: jest.fn().mockImplementation(({to, ...rest}) =>
+            React.createElement("a", {
+                ...rest,
+                href: to,
+            }),
+        ),
         StaticQuery: jest.fn(),
         useStaticQuery: jest.fn(),
     }
