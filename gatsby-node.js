68ac8f3e3b2ad7120b3cf0affb34a5be3f661041
Brad Garropy <bradgarropy@gmail.com>
2021-03-16 22:19:31 -0500

    upgrade to gatsby v3.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,6 @@
 const path = require("path")
 
-const onCreateWebpackConfig = ({stage, actions}) => {
-    if (stage.startsWith("develop")) {
-        actions.setWebpackConfig({
-            resolve: {
-                alias: {"react-dom": "@hot-loader/react-dom"},
-            },
-        })
-    }
-
+const onCreateWebpackConfig = ({actions}) => {
     actions.setWebpackConfig({
         resolve: {
             modules: [path.resolve(__dirname, "src"), "node_modules"],
